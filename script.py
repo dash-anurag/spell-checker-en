ed0aafd77fedcf6d33704609c04011ff36ca692e
Anurag Dash <anuragjeypore1234@gmail.com>
2020-11-02 15:44:48 +0530

    Done v1 implementation😍🦾😁

--- script.py
+++ script.py
@@ -1,22 +1,35 @@
 from spellchecker import SpellChecker
+import re
 
+# creating spellchecker object
 spell = SpellChecker()
 
-# misspelled = spell.unknown(['let', 'us', 'wlak', 'on', 'the', 'groun'])
-#
-# for word in misspelled:
-#     print(spell.correction(word))
-#
-#     print(spell.candidates(word))
-
+# creating file object
 f = open("input.txt", 'r')
+
+# importing contents of input.txt file into data
 data = f.read()
-sentences = data.split("\n")
+
+# dividing the contents of input.txt file into list of sentences with the delimiters being "." and "\n"
+sentences = re.split('; |, |\.|\n', data)
+
+# removing empty list elements formed from the above operation
+while "" in sentences:
+    sentences.remove("")
+# print(sentences)
+
+# for loop for looping over the list of sentences
 for sentence in sentences:
-    correct_sentence = []
-    words = sentence.split(" ")
+    # creating a temp list for storing the corrected words
+    correct_word = []
+    # using strip() to clear any white spaces from the list element and them splitting them into individual words
+    words = sentence.strip().split(" ")
     # print(words)
+    # applying the correction function from the pyspellchecker library and them adding them to the temp list
     for word in words:
-        correct_sentence.append(spell.correction(word))
-    print(" ".join(correct_sentence))
+        correct_word.append(spell.correction(word))
+    # finally joining the individual words and displaying the correctly spelled words to form a sentence
+    print(" ".join(correct_word) + ".")
+
+# closing the file
 f.close()
