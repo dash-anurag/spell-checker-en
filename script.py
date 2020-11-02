332a5cc5517349bab924542f4bbd0094e634cb32
Anurag Dash <anuragjeypore1234@gmail.com>
2020-11-02 14:38:32 +0530

    Created the logic for printing the correct sentence on screen🤞🤞🤞

--- script.py
+++ script.py
@@ -1,13 +1,22 @@
 from spellchecker import SpellChecker
 
-spell = SpellChecker(distance=1)
+spell = SpellChecker()
 
+# misspelled = spell.unknown(['let', 'us', 'wlak', 'on', 'the', 'groun'])
+#
+# for word in misspelled:
+#     print(spell.correction(word))
+#
+#     print(spell.candidates(word))
 
-misspelled = spell.unknown(['let', 'us', 'wlak','on','the','groun'])
-
-for word in misspelled:
-    
-    print(spell.correction(word))
-
-    
-    print(spell.candidates(word))
+f = open("input.txt", 'r')
+data = f.read()
+sentences = data.split("\n")
+for sentence in sentences:
+    correct_sentence = []
+    words = sentence.split(" ")
+    # print(words)
+    for word in words:
+        correct_sentence.append(spell.correction(word))
+    print(" ".join(correct_sentence))
+f.close()
